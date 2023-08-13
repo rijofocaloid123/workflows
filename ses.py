a35f318e0bcbbb9f09c9739f190c201de32605f5
rijofocaloid123 <141813966+rijofocaloid123@users.noreply.github.com>
2023-08-14 00:25:49 +0530

    Update ses.py

--- ses.py
+++ ses.py
@@ -5,10 +5,18 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.application import MIMEApplication
 import shutil
+import json
 
-# Set your AWS credentials
-aws_access_key = 'AKIA6LBJHUQAJT5JTVYC'
-aws_secret_key = 'JHvBAaj+Z34udBH2zi44eTvyKIrkBNqml5fNW4kb'
+ 
+
+# Read AWS credentials from the file
+with open('aws_credentials.json') as f:
+    aws_credentials = json.load(f)
+
+ 
+
+aws_access_key = aws_credentials.get('aws_access_key_id')
+aws_secret_key = aws_credentials.get('aws_secret_access_key')
 region = 'us-east-1'  # Change to your desired AWS region 
 
 # Set email parameters
